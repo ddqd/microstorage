b3dbc31cfea44a5049c58b20619667977fc2bd7d
Dmitry Gureev <dmi.gureev@gmail.com>
2013-11-01 15:53:57 +0400

    create table

--- apps/microstorage/src/microstorage_db_srv.erl
+++ apps/microstorage/src/microstorage_db_srv.erl
@@ -3,6 +3,8 @@
 -behaviour(gen_server).
  
 -export([start_link/0]).
+
+-export([install/0]).
  
 %% gen_server callbacks
 -export([init/1,
@@ -14,7 +16,7 @@
  
 -record(state, {}).
 
--record(storage, {name=[], content=[]}).
+-record(storage, {name, content=[]}).
 
 -record(store, {uuid, storage=#storage{}}).
 
@@ -26,7 +28,7 @@ init([]) ->
  
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
- 
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
  
@@ -40,3 +42,28 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
  
 %% Internal functions
+
+create_table() ->
+    case mnesia:create_table(store,
+                [{attributes, record_info(fields, store)},
+                {index, []},
+                {storage_properties,[   
+                    {ets, [compressed]}, 
+                    {dets, [{auto_save, 3000}]} 
+                ]}, {disc_copies, [node()]}]) of
+        {aborted, {already_exists, Name}} -> lager:log(info, self(), "Table ~p Already Exist", [Name]);
+        {atomic,ok} -> lager:log(info, self(), "Table Created");
+        {aborted, Reason} -> lager:log(error, self(), "Create table Error ~p", [Reason])
+    end,
+    ok.
+
+install() ->
+mnesia:stop(),
+mnesia_schema:delete_schema([node()]),
+case  mnesia:create_schema([node()]) of 
+    ok ->
+        mnesia:start(),
+        create_table();
+    Error -> lager:log(info, self(), "Create schema error ~p", [Error])
+end,
+ok.
